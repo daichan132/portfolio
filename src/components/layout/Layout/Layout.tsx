e6cb58a6717d99126b03d4809a7c6fe2325a2e17
daichan132 <wakadai0319@gmail.com>
2023-03-24 04:06:39 +0900

    背景を修正

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -18,7 +18,7 @@ const useStyles = createStyles(() => ({
       'linear-gradient(0deg, transparent calc(100% - 1px), #f0f0f0 calc(100% - 1px)), linear-gradient(90deg, transparent calc(100% - 1px), #f0f0f0 calc(100% - 1px))',
     backgroundSize: '30px 30px',
     backgroundRepeat: 'repeat',
-    backgroundPosition: 'center center',
+    backgroundPosition: 'left top',
   },
 }));
 
@@ -44,26 +44,26 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <Header />
 
       <SmoothScroll>
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.main
-            variants={variants}
-            initial="hidden"
-            animate="enter"
-            exit="exit"
-            transition={{
-              duration: 0.4,
-            }}
-            key={path}
-            onMouseEnter={() => {
-              setCursorData({ cursorVariant: 'default' });
-            }}
-          >
-            <div className={classes.main}>
+        <div className={classes.main}>
+          <AnimatePresence mode="wait" initial={false}>
+            <motion.main
+              variants={variants}
+              initial="hidden"
+              animate="enter"
+              exit="exit"
+              transition={{
+                duration: 0.4,
+              }}
+              key={path}
+              onMouseEnter={() => {
+                setCursorData({ cursorVariant: 'default' });
+              }}
+            >
               {children}
               {path !== '/' && <Footer />}
-            </div>
-          </motion.main>
-        </AnimatePresence>
+            </motion.main>
+          </AnimatePresence>
+        </div>
       </SmoothScroll>
     </>
   );
